4ecc47a993dba820e63c54cbe639384a7e3c3a91
RobotMafiaCTF <57519066+RobotMafiaCTF@users.noreply.github.com>
2021-12-23 23:36:08 -0500

    Update hello.asm

--- assembly/assembly_labs/lab1/hello.asm
+++ assembly/assembly_labs/lab1/hello.asm
@@ -8,10 +8,10 @@ hello_size: db 12
 section .text 
 _start: 
 
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, hello
-	mov dl, [hello_size]
+	mov eax, 4 ; move syscall number into eax
+	mov ebx, 1 ; move fd into ebx. fd == 1 means we are writing to stdout
+	mov ecx, hello ; move hello string ptr into ecx
+	mov dl, [hello_size] ; move hello world size into edx
 	int 0x80
 	
 	mov al, 1 ; move syscall number into eax
